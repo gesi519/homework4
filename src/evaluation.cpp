7f79874b7627d3c72e10e2d40b9281688fe865ed
gesi <15733996517@163.com>
2024-12-15 15:26:20 +0800

    complete the first work and we can operate with it

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -10,70 +10,118 @@
 extern std :: map<std :: string, ExprType> primitives;
 extern std :: map<std :: string, ExprType> reserved_words;
 
-Value Let::eval(Assoc &env) {} // let expression
+Value Let::eval(Assoc &env) { // let expression
+    return Value(nullptr);
+}
+Value Lambda::eval(Assoc &env) { // lambda expression
+    return Value(nullptr);
+}
+Value Apply::eval(Assoc &e) { // for function calling
+    return Value(nullptr);
+}
+Value Letrec::eval(Assoc &env) { // letrec expression
+    return Value(nullptr);
+}
+Value Var::eval(Assoc &e) { // evaluation of variable
+    return Value(nullptr);
+}
+Value Fixnum::eval(Assoc &e) { // evaluation of a fixnum
+    return Value(new Integer(n));
+}
+Value If::eval(Assoc &e) { // if expression
+    return Value(nullptr);
+}
+Value True::eval(Assoc &e) { // evaluation of #t
+    return Value(nullptr);
+}
+Value False::eval(Assoc &e) { // evaluation of #f
+    return Value(nullptr);
+}
+Value Begin::eval(Assoc &e) { // begin expression
+    return Value(nullptr);
+}
+Value Quote::eval(Assoc &e) { // quote expression
+    return Value(nullptr);
+}
+Value MakeVoid::eval(Assoc &e) { // (void)
+    return Value(nullptr);
+}
+Value Exit::eval(Assoc &e) { // (exit)
+    return Value(new Terminate());
+}
+Value Binary::eval(Assoc &e) { // evaluation of two-operators primitive
+    
+    Value v1 = rand1 -> eval(e);
+    Value v2 = rand2 -> eval(e);
+    
+    return evalRator(v1,v2);
+}
+Value Unary::eval(Assoc &e) { // evaluation of single-operator primitive
+    return Value(nullptr);
+}
+Value Mult::evalRator(const Value &rand1, const Value &rand2) { // *
+    Integer* tmp1 = dynamic_cast<Integer*>(rand1.get());
+    Integer* tmp2 = dynamic_cast<Integer*>(rand2.get());
 
-Value Lambda::eval(Assoc &env) {} // lambda expression
+    return Value(new Integer(tmp1-> n * tmp2 -> n));
+}
+Value Plus::evalRator(const Value &rand1, const Value &rand2) { // +
+    Integer* tmp1 = dynamic_cast<Integer*>(rand1.get());
+    Integer* tmp2 = dynamic_cast<Integer*>(rand2.get());
 
-Value Apply::eval(Assoc &e) {} // for function calling
+    return Value(new Integer(tmp1-> n + tmp2 -> n));
+}
+Value Minus::evalRator(const Value &rand1, const Value &rand2) { // -
+    Integer* tmp1 = dynamic_cast<Integer*>(rand1.get());
+    Integer* tmp2 = dynamic_cast<Integer*>(rand2.get());
 
-Value Letrec::eval(Assoc &env) {} // letrec expression
-
-Value Var::eval(Assoc &e) {} // evaluation of variable
-
-Value Fixnum::eval(Assoc &e) {} // evaluation of a fixnum
-
-Value If::eval(Assoc &e) {} // if expression
-
-Value True::eval(Assoc &e) {} // evaluation of #t
-
-Value False::eval(Assoc &e) {} // evaluation of #f
-
-Value Begin::eval(Assoc &e) {} // begin expression
-
-Value Quote::eval(Assoc &e) {} // quote expression
-
-Value MakeVoid::eval(Assoc &e) {} // (void)
-
-Value Exit::eval(Assoc &e) {} // (exit)
-
-Value Binary::eval(Assoc &e) {} // evaluation of two-operators primitive
-
-Value Unary::eval(Assoc &e) {} // evaluation of single-operator primitive
-
-Value Mult::evalRator(const Value &rand1, const Value &rand2) {} // *
-
-Value Plus::evalRator(const Value &rand1, const Value &rand2) {} // +
-
-Value Minus::evalRator(const Value &rand1, const Value &rand2) {} // -
-
-Value Less::evalRator(const Value &rand1, const Value &rand2) {} // <
-
-Value LessEq::evalRator(const Value &rand1, const Value &rand2) {} // <=
-
-Value Equal::evalRator(const Value &rand1, const Value &rand2) {} // =
-
-Value GreaterEq::evalRator(const Value &rand1, const Value &rand2) {} // >=
-
-Value Greater::evalRator(const Value &rand1, const Value &rand2) {} // >
-
-Value IsEq::evalRator(const Value &rand1, const Value &rand2) {} // eq?
-
-Value Cons::evalRator(const Value &rand1, const Value &rand2) {} // cons
-
-Value IsBoolean::evalRator(const Value &rand) {} // boolean?
-
-Value IsFixnum::evalRator(const Value &rand) {} // fixnum?
-
-Value IsSymbol::evalRator(const Value &rand) {} // symbol?
-
-Value IsNull::evalRator(const Value &rand) {} // null?
-
-Value IsPair::evalRator(const Value &rand) {} // pair?
-
-Value IsProcedure::evalRator(const Value &rand) {} // procedure?
-
-Value Not::evalRator(const Value &rand) {} // not
-
-Value Car::evalRator(const Value &rand) {} // car
-
-Value Cdr::evalRator(const Value &rand) {} // cdr
+    return Value(new Integer(tmp1-> n - tmp2 -> n));
+}
+Value Less::evalRator(const Value &rand1, const Value &rand2) { // <
+    return Value(nullptr);
+}
+Value LessEq::evalRator(const Value &rand1, const Value &rand2) { // <=
+    return Value(nullptr);
+}
+Value Equal::evalRator(const Value &rand1, const Value &rand2) { // =
+    return Value(nullptr);
+}
+Value GreaterEq::evalRator(const Value &rand1, const Value &rand2) { // >=
+    return Value(nullptr);
+}
+Value Greater::evalRator(const Value &rand1, const Value &rand2) { // >
+    return Value(nullptr);
+}
+Value IsEq::evalRator(const Value &rand1, const Value &rand2) { // eq?
+    return Value(nullptr);
+}
+Value Cons::evalRator(const Value &rand1, const Value &rand2) { // cons
+    return Value(nullptr);
+}
+Value IsBoolean::evalRator(const Value &rand) { // boolean?
+    return Value(nullptr);
+}
+Value IsFixnum::evalRator(const Value &rand) { // fixnum?
+    return Value(nullptr);
+}
+Value IsSymbol::evalRator(const Value &rand) { // symbol?
+    return Value(nullptr);
+}
+Value IsNull::evalRator(const Value &rand) { // null?
+    return Value(nullptr);
+}
+Value IsPair::evalRator(const Value &rand) { // pair?
+    return Value(nullptr);
+}
+Value IsProcedure::evalRator(const Value &rand) { // procedure?
+    return Value(nullptr);
+}
+Value Not::evalRator(const Value &rand) { // not
+    return Value(nullptr);
+}
+Value Car::evalRator(const Value &rand) { // car
+    return Value(nullptr);
+}
+Value Cdr::evalRator(const Value &rand) { // cdr
+    return Value(nullptr);
+}
--- src/parser.cpp
+++ src/parser.cpp
@@ -18,9 +18,13 @@ using std :: pair;
 extern std :: map<std :: string, ExprType> primitives;
 extern std :: map<std :: string, ExprType> reserved_words;
 
-Expr Syntax :: parse(Assoc &env) {}
+Expr Syntax :: parse(Assoc &env) {
+    ptr -> parse(env);
+}
 
-Expr Number :: parse(Assoc &env) {}
+Expr Number :: parse(Assoc &env) {
+    return Expr(new Fixnum(n));
+}
 
 Expr Identifier :: parse(Assoc &env) {}
 
@@ -28,6 +32,42 @@ Expr TrueSyntax :: parse(Assoc &env) {}
 
 Expr FalseSyntax :: parse(Assoc &env) {}
 
-Expr List :: parse(Assoc &env) {}
+//这里是对应输入中含有括号的情况
+Expr List :: parse(Assoc &env) {
+    List* list_ =dynamic_cast<List*>(this);
+    int n = list_ -> stxs.size();
+    if(n==0) {
+        throw RuntimeError("");
+    }
+    SyntaxBase* operator_Syntax = (list_ -> stxs[0]).get();
+    std::vector<Expr> content;
+
+    if(Identifier* transform_Syntax = dynamic_cast<Identifier*>(operator_Syntax)) {
+        std::string str = transform_Syntax -> s;
+        if(str == "exit"){
+            return Expr(new Exit());
+        }else if(str == "+"|| str == "-" || str == "*") {
+            if(n != 3) {    // 默认操作符之后只有两个数字
+                throw RuntimeError("");
+            }
+            for(int i = 1;i < n; ++i) {
+                content.push_back((list_ -> stxs[i]).get() ->parse(env));
+            }
+            
+            if(str == "+") {
+                return Expr(new Plus(content[0],content [1]));
+            }else if (str == "-") {
+                return Expr(new Minus(content[0],content [1]));
+            }else if (str == "*") {
+                return Expr(new Mult(content[0],content [1]));
+            }
+        }
+        
+    }else if (Number* transform_Syntax = dynamic_cast<Number*>(operator_Syntax)) {
+
+    }
+    
+
+}
 
 #endif
